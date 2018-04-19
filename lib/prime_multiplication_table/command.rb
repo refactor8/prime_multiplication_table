a2721edd83dfa1882cfe6b7d66135bc616cf1583
Ryan Flores <rfloper@icloud.com>
2018-04-19 20:49:11 +0200

    Check for empty argv values

--- lib/prime_multiplication_table/command.rb
+++ lib/prime_multiplication_table/command.rb
@@ -2,7 +2,7 @@ module PrimeMultiplicationTable
   class Command
     def self.process(command)
       case command.to_s
-      when /(?<number>\d+) (?<algorithm>\w+)\Z/i
+      when /(?<number>\d+)\s*(?<algorithm>\w+)?\Z/i
         [:build, $~[:number].to_i, $~[:algorithm]]
       else
         [:invalid]
